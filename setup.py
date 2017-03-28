e96e6db83d7f211df28fbe430251d845f3a1082a
Étienne BERSAC <etienne.bersac@dalibo.com>
2017-03-28 16:10:03 +0200

    Importable setup.py

--- setup.py
+++ setup.py
@@ -17,22 +17,25 @@ except subprocess.CalledProcessError:
             VERSION = line.replace('Version: ', '').strip()
             break
 
-setup(
-    name='temboard-agent',
-    version=VERSION,
-    author='Dalibo',
-    license='PostgreSQL',
-    packages=find_packages(),
-    scripts=[
-        'temboard-agent', 'temboard-agent-password', 'temboard-agent-adduser'
-    ],
-    url='http://temboard.io/',
-    description='Administration & monitoring PostgreSQL agent.',
-    long_description=open('README.rst').read(),
-    data_files=[('share/temboard-agent/', [
-        'share/temboard-agent.conf.sample',
-        'share/temboard-agent_CHANGEME.pem',
-        'share/temboard-agent_CHANGEME.key',
-        'share/temboard-agent_ca_certs_CHANGEME.pem',
-        'share/temboard-agent.logrotate'
-    ])])
+if __name__ == '__main__':
+    setup(
+        name='temboard-agent',
+        version=VERSION,
+        author='Dalibo',
+        license='PostgreSQL',
+        packages=find_packages(),
+        scripts=[
+            'temboard-agent',
+            'temboard-agent-adduser',
+            'temboard-agent-password',
+        ],
+        url='http://temboard.io/',
+        description='Administration & monitoring PostgreSQL agent.',
+        long_description=open('README.rst').read(),
+        data_files=[('share/temboard-agent/', [
+            'share/temboard-agent.conf.sample',
+            'share/temboard-agent_CHANGEME.pem',
+            'share/temboard-agent_CHANGEME.key',
+            'share/temboard-agent_ca_certs_CHANGEME.pem',
+            'share/temboard-agent.logrotate'
+        ])])
